5b316c3a3d81b451cc62cfd3b75aa55d98e6c232
mmullerx4 <mmullerx4@gmail.com>
2024-06-29 22:33:17 -0400

    06/29/24b

---
+++ src/js/header.mjs
@@ -0,0 +1,16 @@
+const currentPage = window.location.pathname;
+
+switch (currentPage) {
+  case "/page1.html":
+    imagePath = "path/image1.jpg"
+    break;
+  case "/page2.html":
+    imagePath = "path/image2.jpg"
+    break;
+  default:
+    imagePath = "path/default.jpg";
+}
+
+//set the image source diynamically 
+document.querySelector(".logo img").src = imagePath;
+
