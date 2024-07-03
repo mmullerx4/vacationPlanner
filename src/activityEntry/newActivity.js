6e915f93dcef284308ac6c64c15afec493ce2c09
mmullerx4 <mmullerx4@gmail.com>
2024-07-03 16:37:50 -0400

    updateNewActivity & work on js

--- src/activityEntry/newActivity.js
+++ src/activityEntry/newActivity.js
@@ -1,4 +1,12 @@
 //javascript for handling new activity entry
 
-
 //take from form and add to activities.json??
+
+document.getElementById("activityForm").addEventListener("submit", function(event)) {
+  event.preventDefault();
+
+  //get form values
+  const activityName= document.
+
+  //Find the correct date cell and add activity
+}
